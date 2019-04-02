4400aa57bf0da06663a8f5b93407efae270457aa
TheGingeraffe <37917265+TheGingeraffe@users.noreply.github.com>
2019-04-02 11:54:47 -0700

    Create mail_checker.sh

---
+++ mail_checker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# TODO Write some regex that captures domain names in $HOME/mail
+
+# TODO Display all addresses for each domain found in $HOME/mail by checking shadow + passwd
+
+# TODO Check for orphaned entries in shadow/passwd/mail
+
+# TODO Display size (breakdown and total), quotas, mail count (per box and total)
+
+# TODO Do MX check (IP , remote/local)
+
+# TODO Check for forwarders, autoresponders, filters
+
+# TODO Check for exim/dovecot logs if possible
+
+# TODO Check for top recipients/senders
+
+# TODO Check and warn for incorrect perms
