692473f837706256a32d7af829444e5b8d95ec80
TheGingeraffe <hestonio@gmail.com>
2019-04-03 18:41:32 -0600

    Tinkering with storing data in arrays

--- mail_checker.sh
+++ mail_checker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TODO Arrange (several?) array(s) to store all the domain names and info about them
+
+declare -a domains
+declare -A addresses
+
 # TODO Write some regex that captures domain names in $HOME/mail
 
 # Run find from $HOME so output looks like $HOME/etc/<stuff>
@@ -10,13 +15,28 @@
 
 # TODO Display all addresses for each domain found in $HOME/mail by checking shadow + passwd
 
-for DOMAIN in $(find $HOME/etc/ -regextype posix-egrep -regex '(\/\w+){4}\.(((?!rc)\w+\.\w+)|\w+$)' | cut -d'/' -f5); do 
-  for PASSWD_ADDRESS in $(awk -F':' '{print $1}' $HOME/etc/$DOMAIN/passwd); do
-      for SHADOW_ADDRESS in $(awk -F':' '{print $1}' $HOME/etc/$DOMAIN/shadow); do
-        [[ $PASSWD_ADDRESS == $SHADOW_ADDRESS ]] && \
-        echo $DOMAIN - $PASSWD_ADDRESS
-      done  
+
+# Checks $HOME/mail for domains and adds them to the domains array
+
+for DOMAIN in $(find $HOME/etc/ -regextype posix-egrep -regex '(\/\w+){4}\.(((?!rc)\w+\.\w+)|\w+$)' | cut -d'/' -f5); do
+  domains+=($DOMAIN)
+done
+
+# Iterates through domains array and assigns a list of addresses
+# that pass certain checks to their domain in the addresses array
+
+for DOMAIN in "${domains[@]}"; do
+  for ADDRESS_SHADOW in $(awk -F':' '{print $1}' $HOME/etc/$DOMAIN/shadow); do
+    for ADDRESS_PASSWD in $(awk -F':' '{print $1}' $HOME/etc/$DOMAIN/shadow); do
+      [[ $ADDRESS_SHADOW == $ADDRESS_PASSWD ]] && \
+      [[ -e $HOME/mail/$ADDRESS_SHADOW ]] && \ 
+      addresses[$DOMAIN]="$ADDRESS_PASSWD"
+    done
   done
+done
+
+for i in "${!addresses[@]}"; do
+  printf "%s\t%s\n" "${addresses[$i]}""@""$i"
 done
 
 # TODO Check for orphaned entries in shadow/passwd/mail
