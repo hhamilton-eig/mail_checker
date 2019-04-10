08b24dd4d36bdcd5b37dc904ce66c3bc41ffc602
TheGingeraffe <37917265+TheGingeraffe@users.noreply.github.com>
2019-04-09 20:47:52 -0700

    Update mail_checker.sh

--- mail_checker.sh
+++ mail_checker.sh
@@ -1,95 +1,132 @@
 #!/bin/bash
 
-# Arrays for holding all the repetitive data
+## Arrays ##
 
-declare -a domains
-declare -A addresses
+declare -A domain_info
 
-# Could make an associative array on a per-domain basis that holds a bunch of strings 
-# each representing some feature of the domain
-# e.g. - domain_info[$DOMAIN]="admin|info|contact, total storage , perms check , DNS info/checks"
-# Can store addresses in there too! This way the value can be pulled from the array to do operations
+## Functions ##
 
-# Checks $HOME/mail for domains and adds them to the domains array
+# Prints a list of domains present in $HOME/mail
 
-for DOMAIN in $(find $HOME/etc/ -regextype posix-egrep -regex '(\/\w+){4}\.(((?!rc)\w+\.\w+)|\w+$)' \
-| cut -d'/' -f5); do
-  domains+=("$DOMAIN")
+get_domains(){
+  find $HOME/etc/ -regextype posix-egrep -regex '(\/\w+){4}\.(((?!rc )\w+\.\w+)|\w+$)' \
+  | cut -d'/' -f5
+}
+
+# Prints a list of addresses present in $HOME/mail
+
+get_addresses(){
+  find $HOME/mail/*/ -maxdepth 1 -regextype posix-egrep  -regex '(\/\w+){4}\.\w+\/\S+' \
+  | awk 'BEGIN {FS="/"; OFS="@"} {print $6, $5}'
+}
+
+# Prints DNS info with trailing "." trimmed
+# get_mx $domain $record
+
+get_dns(){
+  dig +nocmd $1 $2 +multiline +noall +answer | sed -r 's/\.(\s|$)/ /g'
+}
+
+# TODO make this output nicer
+# Grabs and prints sizes
+
+get_size(){
+        du -sh $HOME/mail/$1/$2;
+}
+
+# Runs some checks on addresses in shadow and passwd for a given domain
+# hash_checker domain (passwd|shadow) (passwd|shadow)
+# TODO add perm checking to these 2 files
+
+hash_checker(){
+  for address in $(awk -F':' '{print $1}' $HOME/etc/$1/$2); do
+    if grep -q $address $HOME/etc/$domain/$3 && \
+    [[ -e $HOME/mail/$1/$address ]]; then
+      continue
+    else
+      echo -e "\n$address@$domain may have issues. Check that"\
+      "address exists in $HOME/mail/${1}, has entries in"\
+      "$HOME/etc/$1/{shadow,passwd}, and that permissions are correct."
+    fi
+  done
+}
+
+# Iterates through output of get_addresses and pairs address names with 
+# their domain in the domain_info array
+
+# TODO find a way to look in passwd, shadow, and maildir for addresses in this list
+# TODO find a way to capture domain list from /var/cpanel/userdata/$(whoami)/main
+
+for i in $(get_addresses); do
+  address="$(echo $i | cut -d'@' -f1)"
+  domain="$( echo $i | cut -d'@' -f2)"
+  domain_info[$domain]+="$address "
 done
 
-# TODO Do MX check (IP , remote/local, SPF/DKIM)
+# Prints addresses in $HOME/mail under each domain
 
-# Iterates through domains array and assigns a list of addresses
-# that pass certain checks to their domain in the addresses array
-
-for DOMAIN in "${domains[@]}"; do
-  for ADDRESS_SHADOW in $(awk -F':' '{print $1}' $HOME/etc/$DOMAIN/shadow); do
-    for ADDRESS_PASSWD in $(awk -F':' '{print $1}' $HOME/etc/$DOMAIN/shadow); do
-      [[ $ADDRESS_SHADOW == $ADDRESS_PASSWD ]] && \
-      [[ -e $HOME/mail/$DOMAIN/$ADDRESS_SHADOW ]] && \
-      addresses[$DOMAIN]="$ADDRESS_PASSWD"
-    done
+for domain in "${!domain_info[@]}"; do
+  echo -e "\nThese addresses are present in ${HOME}/mail/${domain}:"
+  echo
+  for address in $(echo "${domain_info[$domain]}"); do
+    echo "${address}@${domain}"
   done
 done
 
-echo -e "\nIf an address does not appear in this list, check" \
-" $HOME/etc/<domain>/{shadow,passwd} for the missing entries, $HOME/mail/<domain> for content," \
-" and potentially run mailperm.\n"
-for i in "${!addresses[@]}"; do
-  echo -e "${addresses[$i]}""@""$i"
-done
+# Iterates through domains and performs shadow/passwd file checks
 
-# Checks for orphaned entries in $HOME/etc/$DOMAIN/{shadow,passwd}
-# TODO add uapi loop that recreates addresses if user says "yes" for each domain
-
-for DOMAIN in "${domains[@]}"; do
-  if [[ $(wc -l $HOME/etc/$DOMAIN/shadow) == $(wc -l $HOME/etc/$DOMAIN/passwd) ]]; then
-    echo -e "\nLength of shadow and passwd files match."
+for domain in "${!domain_info[@]}"; do
+  if [[ -e $HOME/etc/$domain/passwd ]] && \
+  [[ -e $HOME/etc/$domain/shadow ]]; then
+    hash_checker $domain shadow passwd
+    hash_checker $domain passwd shadow
   else
-    echo -e "\nThe length of the shadow and passwd files for $DOMAIN do not match." \
-" Would you like to recreate the addresses in the longer of the two files?" \
-" Warning! This will reset passwords for all email addresses under this domain," \
-" so only do it if you are sure."
+    echo -e "\nThere is something wrong with the shadow/passwd files for ${domain}." \
+    "Check that they exist and that permissions are correct.\n"
   fi
 done
 
-# Functions for grabbing sizes
-
-domain_size(){
-	du -sh $HOME/mail/$DOMAIN;
-}
-
-address_size(){
-	du -sh $HOME/mail/$DOMAIN/$ADDRESS;
-}
+# TODO add uapi loop that recreates addresses if user says "yes" for each domain
 
 # Total size of each domain
 
 echo -e "\nTotal mail size by domain:\n"
 
-for DOMAIN in "${domains[@]}"; do
-  echo "$(domain_size)"
+for domain in "${!domain_info[@]}"; do
+  echo "$(get_size $domain)"
 done
 
-# TODO quotas, mail count for each box, inbox/sent/trash DU breakdown
-# TODO Check for forwarders, autoresponders, filters
+# Total size of each address in $HOME/mail
 
 echo -e "\nTotal mail size by address:\n"
 
-for DOMAIN in "${domains[@]}"; do
-  for ADDRESS in \
-  $(find $HOME/mail/$DOMAIN -maxdepth 1 -regextype posix-egrep  -regex '(\/\w+){4}\.\w+\/.*' | cut -d'/' -f6); do
-  address_size
+for domain in "${!domain_info[@]}"; do
+  for address in $(echo "${domain_info[$domain]}"); do
+    echo "${address}@${domain} - $(get_size $domain $address)"
   done
   echo
 done
 
-# Can't do before addresses array gets refactored to include all addresses
-#for ADDRESS in "${addresses[@]}"; do
-#  echo "$(address_size)"
-#done
+# TODO quotas, mail count for each box, inbox/sent/trash DU breakdown
 
-echo
+
+# TODO Check for forwarders, autoresponders, filters
+# Forwarders and autoresponders are found in /etc/valiases/${domain}
+# Filters are found in /etc/vfilters/${domain}
+
+
+# Checks and prints NS + MX info for domains in array
+# Find authoritative NS from WHOIS, do a cURL to workaround broke whois command
+
+for domain in "${!domain_info[@]}"; do
+  echo -e "DNS checks for ${domain}:\n"
+  get_dns $domain ns
+  echo
+  get_dns $domain mx
+  echo
+done
+
+# TODO Check remote/local, SPF/DKIM
 
 # TODO Check for exim/dovecot logs if possible
 
