a4f15439ac80d4bab320a7d1d9f6a7ed3d835813
TheGingeraffe <37917265+TheGingeraffe@users.noreply.github.com>
2019-04-18 14:06:40 -0700

    Update spf_check.sh

--- spf_check.sh
+++ spf_check.sh
@@ -6,7 +6,7 @@ spf="$(dig $1 txt | awk -F'"' '/spf/{print $2}')"
 
 # Checks if SPF is greater than 0 chars
 
-if [[ $spf | head -c1 | wc -c) -ne 0 ]]; then
+if [[ $($spf | head -c1 | wc -c) -ne 0 ]]; then
   echo -e "$1's SPF - \n$spf"
 else
   echo -e "\n$1 does not appear to have an SPF configured!"
